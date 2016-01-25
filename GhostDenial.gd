78c19e98df1acebb3b68b706c7cf50c937192c87
Ryan Goldstein <ryanisaacg@gmail.com>
2016-01-25 11:59:46 -0500

    Added a timer on the teleportation to prevent double-collision

--- GhostDenial.gd
+++ GhostDenial.gd
@@ -2,22 +2,41 @@
 extends Area2D
 
 var jumps = 0
+var jump_cooldown = false
+var timer;
 const MAX_JUMPS = 10
 var parent;
+
 func _ready():
 	# Initialization here
 	parent = get_node("..")
-
+	timer = Timer.new()
+	add_child(timer)
+	timer.set_wait_time(0.5)
+	timer.connect("timeout",self,"clear_cooldown")
+	
+func clear_cooldown():
+	jump_cooldown = false
+	print("CLEAR")
+	timer.stop()
+	
 func jump():
+	if jump_cooldown:
+		return null;
 	if jumps < MAX_JUMPS:
 		var pos = parent.get_pos()
 		if jumps % 2 == 0:
 			pos.y -= 256
 		elif jumps in [3, 7]:
 			pos.x += 900
+			parent.set_flip_h(true);
 		else:
 			pos.x -= 900
+			parent.set_flip_h(false);
 		parent.set_pos(pos)
 		jumps += 1
+		jump_cooldown = true
+		timer.start()
+		
 	else:
 		print("ADVANCE")
