e679afca23ff0d2a4970b1a7a385c2893b8a6adc
Ryan Goldstein <ryanisaacg@gmail.com>
2016-01-28 17:56:39 -0500

    Added arrows that can force you to go right

--- CharacterController.gd
+++ CharacterController.gd
@@ -5,27 +5,22 @@ const WALK_SPEED = 300
 
 var velocity = Vector2()
 var jump_previous = false
+var canMoveRight = true
+var canMoveLeft = true
+var canJump = true
 
 func _fixed_process(delta):
 	velocity.y += delta * GRAVITY
-	if Input.is_action_pressed("ui_left"):
+	if canMoveLeft and Input.is_action_pressed("ui_left"):
 		velocity.x = - WALK_SPEED
 		get_node("AnimatedSprite").set_flip_h(true);
-	elif Input.is_action_pressed("ui_right"):
+	elif canMoveRight and Input.is_action_pressed("ui_right"):
 		velocity.x =   WALK_SPEED
 		get_node("AnimatedSprite").set_flip_h(false);
 	else:
 		velocity.x = 0
-	if Input.is_action_pressed("ui_accept"):
+	if canJump and Input.is_action_pressed("ui_accept"):
 		if not jump_previous: 
-			#var space = get_world_2d().get_space();
-			#var space_state = Physics2DServer.space_get_direct_state( space )
-			#var below = self.get_pos();
-			#below.y += 20
-			#var returnval = space_state.intersect_ray(self.get_pos(), below, [self]);
-			#below.x += -abs(velocity.x) / velocity.x * 16;
-			#var backwards = space_state.intersect_ray(self.get_pos(), below, [self]);
-			#if returnval.size() != 0 or backwards.size() != 0:
 			velocity.y = -450;
 		jump_previous = true;
 	else:
--- ForceDown.gd
+++ ForceDown.gd
@@ -7,12 +7,17 @@ extends Area2D
 
 func _ready():
 	connect("body_enter", self, "force_down")
-
+	connect("body_exit", self, "stop_forcing")
+	
 func force_down(object):
 	if object.get_name() == "Player":
 		var velocity = object.velocity;
 		if velocity.y < 0:
 			velocity.y = -velocity.y;
 		object.velocity = velocity;
+		object.canJump = false
 
+func stop_forcing(object):
+	if object.get_name() == "Player":
+		object.canJump = true
 
